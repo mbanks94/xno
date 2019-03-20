bbf8f7b6601d9f6d21c7064db032afa51d828718
mbanks94 <mbanks123456@gmail.com>
2019-03-20 11:17:10 -0500

    fixed static files path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.json());
-app.use('/', express.static(path.join(__dirname, '../client')));
+app.use('/', express.static(path.join(__dirname, './client')));
 app.use((req, res, next) => {
     console.log(req.url);
     next();
