25fc43aa520907be7f1714f7db9482138be44853
mbanks94 <mbanks123456@gmail.com>
2019-03-20 11:14:28 -0500

    Added express/heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '../client')));
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running');
+});
